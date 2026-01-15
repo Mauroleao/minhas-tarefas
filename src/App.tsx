59dd9e923436da03d6bb7995b3c676fbd088e552
Mauro <mauro_leao@hotmail.com>
2026-01-15 09:23:03 -0300

    finalizando as configurações do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import BarraLateral from './containers/BarraLateral'
+import ListaDeTarefas from './containers/ListaDeTarefas'
+import EstiloGlobal, { Container } from './styles'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <>
+      <EstiloGlobal />
+      <Container>
+        <BarraLateral />
+        <ListaDeTarefas />
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
