229ce10c6e59dfcc088f1e2b8bed8e80869be747
Mauro <seu@email.com>
2026-01-19 10:47:12 -0300

    fix(Tarefa): map baixa/media priorities to colors; fix display typo

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -1,26 +1,48 @@
 import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
+
+type TagProps = {
+  prioridade?: string
+  status?: string
+}
+
+function retornaCorDeFundo(props: TagProps): string {
+  if ('status' in props) {
+    if (props.status === 'pendente') return variaveis.amarelo
+    if (props.status === 'concluida') return variaveis.verde
+  } else if ('prioridade' in props) {
+    if (props.prioridade === 'urgente') return variaveis.vermelho
+    if (props.prioridade === 'importante') return variaveis.amareloZ
+    if (props.prioridade === 'media') return variaveis.amareloZ
+    if (props.prioridade === 'baixa') return variaveis.verde
+  }
+
+  return '#ccc'
+}
 
 export const Card = styled.div`
   background-color: #fcfcfc;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   padding: 16px;
   margin-bottom: 32px;
+  border-radius: 16px;
 `
 
 export const Titulo = styled.h3`
   font-size: 18px;
   font-weight: bold;
+  margin-bottom: 16px;
 `
 
-export const Tag = styled.span`
+export const Tag = styled.span<TagProps>`
   padding: 4px 8px;
   color: #fff;
   font-weight: bold;
   font-size: 10px;
-  background-color: #e1a32a;
+  background-color: ${(props) => retornaCorDeFundo(props)};
   border-radius: 8px;
   margin-right: 16px;
-  diasplay: inline-block;
+  display: inline-block;
 `
 
 export const Descricao = styled.textarea`
@@ -31,6 +53,9 @@ export const Descricao = styled.textarea`
   display: block;
   width: 100%;
   margin-top: 16px;
+  resize: none;
+  border: none;
+  background-color: transparent;
 `
 
 export const BarraAcoes = styled.div`
@@ -48,4 +73,12 @@ export const Botao = styled.button`
   background-color: #2f3640;
   border-radius: 8px;
   margin-right: 8px;
+`
+
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.verde};
+`
+
+export const BotaoCancelarRemover = styled(Botao)`
+  background-color: ${variaveis.vermelho};
 `
