6a5146ae5c25d2f35db844d916738ec4087a136b
Mauro <mauro_leao@hotmail.com>
2026-02-03 11:56:26 -0300

    feat(Filtro): implement filtering functionality and integrate Redux state management

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,21 +1,72 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/Filtrocard'
 
-import * as S from './styles'
+import { alterarTermo } from '../../store/reducers/filtro'
+import { RootReducer } from '../../store'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="Buscar" />
-      <S.Filtros>
-        <FiltroCard legenda="Pendentes" contador={1} ativo={false} />
-        <FiltroCard legenda="Concluidas" contador={2} ativo={false} />
-        <FiltroCard legenda="Urgentes" contador={3} ativo={false} />
-        <FiltroCard legenda="Importantes" contador={4} ativo={false} />
-        <FiltroCard legenda="Normal" contador={5} ativo={false} />
-        <FiltroCard legenda="Todas" contador={10} ativo />
-      </S.Filtros>
-    </div>
-  </S.Aside>
-)
+import * as S from './styles'
+import * as enums from '../../components/utils/enuns/tarefa'
+
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Buscar"
+          value={termo}
+          onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+        />
+        <S.Filtros>
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="status"
+            legenda="Pendentes"
+            contador={1}
+            ativo={false}
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="status"
+            legenda="Concluidas"
+            contador={2}
+            ativo={false}
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            legenda="Urgentes"
+            contador={3}
+            ativo={false}
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="prioridade"
+            legenda="Importantes"
+            contador={4}
+            ativo={false}
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            legenda="Normal"
+            contador={5}
+            ativo={false}
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="todas"
+            legenda="Todas"
+            contador={10}
+            ativo
+          />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,16 +1,51 @@
-import Tarefa from '../../components/Tarefa'
-import { Container } from './styles'
-import { RootReducer } from '../../store'
 import { useSelector } from 'react-redux'
 
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+import { RootReducer } from '../../store'
+
 const ListaDeTarefas = () => {
-  const tarefas = useSelector((state: RootReducer) => state.tarefas)
+  const itens = useSelector((state: RootReducer) => state.tarefas)
+  const { termo, criterio, valor } = useSelector(
+    (state: RootReducer) => state.filtro
+  )
+
+  const filtraTarefas = () => {
+    let tarefasFiltradas = itens
+    if (termo) {
+      return itens.filter(
+        (item) => item.titulo.toLowerCase().search(termo.toLowerCase()) >= 0
+      )
+
+      if (criterio === 'prioridade') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.prioridade === valor
+        )
+      } else if (criterio === 'status') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.status === valor
+        )
+      }
+
+      return tarefasFiltradas
+    } else {
+      return itens
+    }
+  }
 
   return (
     <Container>
-      <p>2 Tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+      <p>
+        2 Tarefas marcadas como: &quot;categoria&quot; e &quot;{termo}&quot;
+      </p>
       <ul>
-        {tarefas.map((t) => (
+        <li>{termo}</li>
+        <li>{criterio}</li>
+        <li>{valor}</li>
+      </ul>
+      <ul>
+        {filtraTarefas().map((t) => (
           <li key={t.id}>
             <Tarefa
               id={t.id}
@@ -26,4 +61,4 @@ const ListaDeTarefas = () => {
   )
 }
 
-export default ListaDeTarefas;
+export default ListaDeTarefas
